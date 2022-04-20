c65df65e3372c270eab897ebfd06413b68e2ee7e
Augusto Dias <me@gutodias.dev>
2022-04-19 23:36:38 -0300

    chore: add next-connect

--- src/backend/data/services/CreateUserService.ts
+++ src/backend/data/services/CreateUserService.ts
@@ -2,6 +2,6 @@ import { CreateUser } from '../../domain/features'
 
 export class CreateUserService implements CreateUser {
   async perform(params: CreateUser.Params): Promise<CreateUser.Result> {
-
+    console.log(params)
   }
 }
